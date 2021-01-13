848e97cb69e2014a6ee6275ce88289fcb25a79c0
class-Kazushi <k.a.z928marine@gmail.com>
2021-01-13 13:08:33 +0900

    カラムをfloat型に変更

--- db/migrate/20210112025054_create_calculations.rb
+++ db/migrate/20210112025054_create_calculations.rb
@@ -1,8 +1,8 @@
 class CreateCalculations < ActiveRecord::Migration[6.0]
   def change
     create_table :calculations do |t|
-      t.integer :weight, null: false
-      t.integer :height, null: false
+      t.float :weight, null: false
+      t.float :height, null: false
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,8 @@
 ActiveRecord::Schema.define(version: 2021_01_12_025054) do
 
   create_table "calculations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "weight", null: false
-    t.integer "height", null: false
+    t.float "weight", null: false
+    t.float "height", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
